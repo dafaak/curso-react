9ceeecc143b36a92a00dae300a57e57789e751b8
Israel <israel.pe@outlook.es>
2022-12-12 12:24:33 -0500

    feat: hello world comp

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+import {HelloWorldApp} from "./HelloWorldApp.jsx";
 
-const App = () => {
-
-    return (<h1>Hola mundo!!</h1>);
-}
+// const App = () => {
+//
+//     return (<h1>Hola mundo!!</h1>);
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App></App>
+        {/*<App></App>*/}
+        <HelloWorldApp></HelloWorldApp>
     </React.StrictMode>
 );
