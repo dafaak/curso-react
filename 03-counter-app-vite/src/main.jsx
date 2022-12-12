d7c98ee12f27841fa3c3b874db05783f5100d168
Israel <israel.pe@outlook.es>
2022-12-12 12:09:54 -0500

    feat: hola mundo 03

---
+++ 03-counter-app-vite/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const App = () => {
+
+    return (<h1>Hola mundo!!</h1>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App></App>
+    </React.StrictMode>
+);
--- README.md
+++ README.md
@@ -8,5 +8,9 @@
 
 ```js
 const divRoot = document.querySelector('#root');
-ReactDOM.render(<h1>Hola mundo<h1/>);
+ReactDOM.render(<h1>Hola mundo</h1>);
 ```
+
+# BABEL
+
+permite usar caracteristicas actuales de js, en navegadores que aun no soportan esas caracteristicas
