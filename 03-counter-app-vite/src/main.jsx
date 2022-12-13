c77f6cc96c92717019b641d724c3fe6c8d519f95
Israel <israel.pe@outlook.es>
2022-12-13 15:05:21 -0500

    feat: comp counter app

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import {HelloWorldApp} from "./HelloWorldApp.jsx";
 import {FirstApp} from "./FirstApp.jsx";
 import './styles.css';
+import {CounterApp} from "./CounterApp.jsx";
 // const App = () => {
 //
 //     return (<h1>Hola mundo!!</h1>);
@@ -11,7 +12,8 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/*<App></App>*/}
-        <HelloWorldApp/>
-        <FirstApp />
+        {/*<HelloWorldApp/>*/}
+        {/*<FirstApp />*/}
+        <CounterApp value={1}></CounterApp>
     </React.StrictMode>
 );
