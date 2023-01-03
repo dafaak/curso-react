a5f3f633d1e072854e0984bfeb285c6c8486ad5e
Israel <israel.pe@outlook.es>
2023-01-03 12:33:44 -0500

    updatE: quito console.log de la prueba de async

--- 03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
+++ 03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
@@ -3,7 +3,6 @@ import {getImagen} from "../../src/base-pruebas/11-async-await.js";
 describe('Pruebas en 11-async-await.js', () => {
         test('getImage debe retornar una URL de la imagen', async () => {
                 const url = await getImagen();
-                console.log(url);
                 expect(typeof url).toBe('string');
             }
         )
